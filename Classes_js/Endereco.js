ebce3851e637cd4606bc047c26b3dc43d3e2cbdf
WindiestPick <gustavosuperfla@gmail.com>
2021-11-22 00:30:31 -0500

    só apaguei

--- Classes_js/Endereco.js
+++
@@ -1,9 +0,0 @@
-class Endereco{
-    constructor(rua, numero, cep, cidade, bairro){
-        this.rua = rua;
-        this.numero = numero;
-        this.cep = cep;
-        this.cidade = cidade;
-        this.bairro = bairro
-    }
-}
